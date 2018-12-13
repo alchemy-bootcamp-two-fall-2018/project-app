db416d01b9ed6852e52b33e974c05d4279089dd5
Cheri Portman <cheriportman@hotmail.com>
2018-12-13 12:43:21 -0800

    app side set up

---
+++ src/components/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Routerview></Routerview>
+  </div>
+</template>
+
+<script>
+import api from '../services/api.js';
+
+export default {
+  data() {
+    return {
+      result: null
+    };
+  },
+  name: 'app',
+  components: { 
+  },
+  created() {
+    api.test().then(result => this.result = result);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <h2>Welcome to My Kewl Pets!</h2>
+    <!-- <p>
+      <RouterLink to="/pets">view pets</RouterLink>
+    </p>
+    <table v-if="stats">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Count</th>
+          <th>Average</th>
+          <th>Min</th>
+          <th>Max</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="stat in stats"
+          :key="stat.petId">
+          <td>{{stat.petName}}</td>
+          <td>{{stat.count}}</td>
+          <td>{{stat.averageScore}}</td>
+          <td>{{stat.minimumScore}}</td>
+          <td>{{stat.maximumScore}}</td>
+        </tr>
+      </tbody>
+    </table> -->
+  </section>
+</template>
+
+<script>
+// import api from '../../services/api';
+export default {
+  
+};
+</script>
+
+<style lang="postcss" scoped>
+table {
+  width: 100%;
+  max-width: 600px;
+  border-collapse: collapse;
+  th, td {
+    text-align: right;
+  }
+  td {
+    padding: 10px 3px;
+  }
+  th:first-child, td:first-child {
+    text-align: left;
+  }
+  tr:nth-child(even) {
+    background: rgba(0, 0, 0, .1);
+  }
+}
+</style>
