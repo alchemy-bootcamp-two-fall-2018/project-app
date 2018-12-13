ce7df639ad55e9dfff8b5fc98aef91577fbfddeb
avanvalk <drewvv@gmail.com>
2018-12-13 12:18:12 -0800

    MVP complete for APP

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <RouterView></RouterView>
+    {{result}}
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+export default {
+  data() {
+    return {
+      result: null
+    };
+  },
+  components: {
+    
+  },
+  created() {
+    api.test()
+      .then(result => this.result = result);
+  }
+};
+</script>
